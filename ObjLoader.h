56fd81e5f4502f7d0a4da5ad33855b7758485537
Jack <jackshen2011@gmail.com>
2017-12-15 00:57:42 +0800

    采用glRotatef函数，结果该方法在使用后会出现旋转和平移先后运动的问题。

---
+++ ObjLoader.h
@@ -0,0 +1,186 @@
+//
+// Created by jack on 17-12-2.
+//
+
+#ifndef BUNNY_VTMOEDEL_H
+#define BUNNY_VTMOEDEL_H
+
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <GL/glut.h>
+
+using namespace std;
+
+class ObjLoader {
+public:
+    ObjLoader() = default;
+
+    explicit ObjLoader(const string &filename) {
+        string line;
+        ifstream f(filename);
+        if (!f.is_open()) {
+            cout << "没有发现OBJ文件" << endl;
+            exit(2);
+        }
+
+        while (!f.eof()) {
+            getline(f, line);//拿到obj文件中一行，作为一个字符串
+            vector<string> parameters;
+            string tailMark = " ", ans;
+
+            line = line.append(tailMark);
+            for (char ch : line) {
+                if (ch != ' ') {
+                    ans += ch;
+                } else {
+                    parameters.push_back(ans); //取出字符串中的元素，以空格切分
+                    ans.clear();
+                }
+            }
+            if (parameters.size() != 4) {
+                cout << "the size is not correct" << endl;
+                exit(3);
+            } else {
+                if (parameters[0] == "v") {   //如果是顶点的话
+                    vector<GLfloat> Point;
+                    for (int i = 1; i < 4; i++) {   //从1开始，将顶点的xyz三个坐标放入顶点vector
+                        auto xyz = (float) strtod(parameters[i].c_str(), nullptr);
+                        Point.push_back(xyz);
+                    }
+                    vSets.push_back(Point);
+                } else if (parameters[0] == "f") {   //如果是面的话，存放三个顶点的索引
+                    vector<GLint> vIndexSets;
+                    for (int i = 1; i < 4; i++) {
+                        string x = parameters[i];
+                        ans.clear();
+                        for (char ch : x) {   //跳过‘/’
+                            if (ch != '/') {
+                                ans += ch;
+                            } else {
+                                break;
+                            }
+                        }
+                        auto index = (int) strtod(ans.c_str(), nullptr);
+                        index--;
+                        vIndexSets.push_back(index);
+                    }
+                    fSets.push_back(vIndexSets);
+                }
+            }
+        }
+        f.close();
+
+    }
+
+    ~ObjLoader() = default;
+
+    void Draw() {
+
+        glRotatef(RotateX, 1.0f, 0.0f, 0.0f);
+        glRotatef(RotateY, 0.0f, 1.0f, 0.0f);
+        glRotatef(RotateZ, 0.0f, 0.0f, 1.0f);
+        glTranslatef(TranslateX, TranslateY, TranslateZ);
+        glScalef(ScaleXYZ, ScaleXYZ, ScaleXYZ);
+
+        glBegin(GL_TRIANGLES);//开始绘制
+        for (auto &fSet : fSets) {
+            GLfloat VN[3];
+            //三个顶点
+            GLfloat SV1[3];
+            GLfloat SV2[3];
+            GLfloat SV3[3];
+            if (fSet.size() != 3) {
+                cout << "the fSetsets_Size is not correct" << endl;
+            } else {
+                GLint firstVertexIndex = fSet[0];//取出顶点索引
+                GLint secondVertexIndex = fSet[1];
+                GLint thirdVertexIndex = fSet[2];
+
+                SV1[0] = (vSets[firstVertexIndex])[0];//第一个顶点
+                SV1[1] = (vSets[firstVertexIndex])[1];
+                SV1[2] = (vSets[firstVertexIndex])[2];
+
+                SV2[0] = (vSets[secondVertexIndex])[0]; //第二个顶点
+                SV2[1] = (vSets[secondVertexIndex])[1];
+                SV2[2] = (vSets[secondVertexIndex])[2];
+
+                SV3[0] = (vSets[thirdVertexIndex])[0]; //第三个顶点
+                SV3[1] = (vSets[thirdVertexIndex])[1];
+                SV3[2] = (vSets[thirdVertexIndex])[2];
+
+
+                GLfloat vec1[3], vec2[3], vec3[3];//计算法向量
+                //(x2-x1,y2-y1,z2-z1)
+                vec1[0] = SV1[0] - SV2[0];
+                vec1[1] = SV1[1] - SV2[1];
+                vec1[2] = SV1[2] - SV2[2];
+
+                //(x3-x2,y3-y2,z3-z2)
+                vec2[0] = SV1[0] - SV3[0];
+                vec2[1] = SV1[1] - SV3[1];
+                vec2[2] = SV1[2] - SV3[2];
+
+                //(x3-x1,y3-y1,z3-z1)
+                vec3[0] = vec1[1] * vec2[2] - vec1[2] * vec2[1];
+                vec3[1] = vec2[0] * vec1[2] - vec2[2] * vec1[0];
+                vec3[2] = vec2[1] * vec1[0] - vec2[0] * vec1[1];
+
+                auto D = (float) sqrt(pow(vec3[0], 2) + pow(vec3[1], 2) + pow(vec3[2], 2));
+
+                VN[0] = vec3[0] / D;
+                VN[1] = vec3[1] / D;
+                VN[2] = vec3[2] / D;
+
+                glNormal3f(VN[0], VN[1], VN[2]);//绘制法向量
+                glVertex3f(SV1[0], SV1[1], SV1[2]);//绘制三角面片
+                glVertex3f(SV2[0], SV2[1], SV2[2]);
+                glVertex3f(SV3[0], SV3[1], SV3[2]);
+            }
+        }
+        glEnd();//拿到obj文件中一行，作为一个字符串
+        vector<string> parameters;
+        string tailMark = " ", ans;
+
+    }
+
+    void changeRotate(float RotateX, float RotateY, float RotateZ) {
+        this->RotateX = RotateX;
+        this->RotateY = RotateY;
+        this->RotateZ = RotateZ;
+    }
+
+    void changeTranslate(float TranslateX, float TranslateY, float TranslateZ) {
+        this->TranslateX = TranslateX;
+        this->TranslateY = TranslateY;
+        this->TranslateZ = TranslateZ;
+    }
+
+    void changeScale(float ScaleXYZ) {
+        this->ScaleXYZ = ScaleXYZ;
+    }
+
+    void changeFalseTrue(bool Translate, bool Rotate) {
+        this->Translate = Translate;
+        this->Rotate = Rotate;
+    }
+
+private:
+    vector<vector<GLfloat >> vSets;
+    vector<vector<GLint>> fSets;
+    float RotateX = 0.0f;
+    float RotateY = 0.0f;
+    float RotateZ = 0.0f;
+    float TranslateX = 0.0f;
+    float TranslateY = 0.0f;
+    float TranslateZ = 0.0f;
+    float ScaleXYZ = 1.0f;
+    bool Rotate = false;
+    bool Translate = false;
+};
+
+
+#endif //BUNNY_VTMOEDEL_H
---
+++ main.cpp
@@ -0,0 +1,171 @@
+//
+// Created by jack on 17-12-2.
+//
+
+#include <iostream>
+#include "ObjLoader.h"
+
+#define  GLUT_WHEEL_UP 3           //定义滚轮操作
+#define  GLUT_WHEEL_DOWN 4
+
+
+string filePath;
+ObjLoader *objModel;
+bool isClicked_Rotate = false;
+bool isClicked_Translate = false;
+int Rotate_temp_X = 0;
+int Rotate_temp_Y = 0;
+int Translate_temp_X = 0;
+int Translate_temp_Y = 0;
+int Rotate_change_x = 0, Rotate_change_y = 0, Translate_change_x = 0, Translate_change_y = 0;
+float Scale_coefficient = 1;
+//安置光源
+void setLightRes() {
+    GLfloat lightPosition[] = {100.0f, 100.0f, 100.0f, 0.0f};
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+    glEnable(GL_LIGHTING); //启用光源
+    glEnable(GL_LIGHT0);   //使用指定灯光
+}
+
+//初始化
+void init() {
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("ObjLoader");
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_SMOOTH);
+    setLightRes();
+    glEnable(GL_DEPTH_TEST);
+}
+
+void display() {
+    objModel->changeTranslate(Translate_change_x, Translate_change_y, 0);
+    objModel->changeScale(Scale_coefficient);
+    objModel->changeRotate(0.0f, Rotate_change_x, Rotate_change_y);
+    glColor3f(1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+//    glTranslatef(0.0f, 0.0f, 0.0f);
+    setLightRes();
+    gluLookAt(-5, 0, 0, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
+    objModel->Draw();//绘制obj模型
+//    if (isClicked_Rotate)
+//        glPopMatrix();
+//    if (isClicked_Translate)
+//        glPopMatrix();
+//    glPopMatrix();
+    glutSwapBuffers();
+    glFlush();
+}
+
+void reshape(int width, int height) {
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0f, (GLdouble) width / (GLdouble) height, 1.0f, 200.0f);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void moseMove(int button, int state, int x, int y) {
+//    cout << "state:" << state << endl;
+//    cout << "button:" << button << endl;
+    if (button == GLUT_LEFT_BUTTON) {
+        switch (state) {
+            case GLUT_DOWN:
+                isClicked_Rotate = !isClicked_Rotate;
+                Rotate_temp_X = x;
+                Rotate_temp_Y = y;
+                break;
+            case GLUT_UP:
+                isClicked_Rotate = !isClicked_Rotate;
+                break;
+            default:
+                break;
+        }
+    }
+    if (button == GLUT_RIGHT_BUTTON) {
+        switch (state) {
+            case GLUT_DOWN:
+                isClicked_Translate = !isClicked_Translate;
+                Translate_temp_X = x;
+                Translate_temp_Y = y;
+                break;
+            case GLUT_UP:
+                isClicked_Translate = !isClicked_Translate;
+                Translate_temp_Y = Translate_temp_X = 0;
+                break;
+            default:
+                break;
+        }
+    }
+    switch (button) {
+        case GLUT_WHEEL_DOWN:
+            Scale_coefficient += 0.1f;
+            break;
+        case GLUT_WHEEL_UP:
+            Scale_coefficient -= 0.1f;
+            if (Scale_coefficient <= 1.0f)
+                Scale_coefficient = 1.0f;
+            break;
+        default:
+            break;
+    }
+
+}
+
+
+void mouseMotion(int x, int y) {
+//    objModel->changeRotate(0, Rotate_change_x, Rotate_change_y);
+    if (isClicked_Rotate) {
+        Rotate_change_x = x - Rotate_temp_X;
+        Rotate_change_y = y - Rotate_temp_Y;
+    }
+    if (isClicked_Translate) {
+        Translate_change_x = (x - Translate_temp_X) / 10;
+        Translate_change_y = (y - Translate_temp_Y) / 10;
+    }
+    objModel->changeFalseTrue(isClicked_Translate, isClicked_Rotate);
+}
+
+
+void keyboardFunction(unsigned char key, int x, int y) {
+
+    switch (key) {
+        case 'r':
+            cout << "key:" << key << endl;
+            Scale_coefficient = 1.0f;
+            Translate_change_y = 0;
+            Translate_change_x = 0;
+            Rotate_change_y = 0;
+            Rotate_change_x = 0;
+            break;
+        case 'q':
+            cout << "program finished!" << endl;
+            exit(0);
+        default:
+            break;
+
+    }
+
+}
+
+void myIdle() {
+    glutPostRedisplay();
+}
+
+
+int main(int argc, char **argv) {
+    filePath = "bunny.obj";
+    objModel = new ObjLoader(filePath);
+    glutInit(&argc, argv);
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMouseFunc(moseMove);
+    glutMotionFunc(mouseMotion);
+    glutKeyboardFunc(keyboardFunction);
+    glutIdleFunc(myIdle);
+    glutMainLoop();
+    return 0;
+}
